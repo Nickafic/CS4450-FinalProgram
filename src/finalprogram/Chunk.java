e4e4f8fc7fecc6ace6f4b04c75bfa0e9a070a766
iinic <nickafic@gmail.com>
2024-05-04 18:41:48 -0700

    Original Terrain

--- src/finalprogram/Chunk.java
+++ src/finalprogram/Chunk.java
@@ -10,10 +10,10 @@ import org.newdawn.slick.opengl.TextureLoader;
 import org.newdawn.slick.util.ResourceLoader;
 
 public class Chunk {
-    
-    static final int CHUNK_SIZE = 60;
+
+    static final int CHUNK_SIZE = 40;
     static final int CUBE_LENGTH = 2;
-    
+
     private Block[][][] Blocks;
     private int VBOVertexHandle;
     private int VBOColorHandle;
@@ -34,54 +34,80 @@ public class Chunk {
         glDrawArrays(GL_QUADS, 0, CHUNK_SIZE * CHUNK_SIZE * CHUNK_SIZE* 24);
         glPopMatrix();
     }
-  
-  public void rebuildMesh(float startX, float startY, float startZ) {
-        
-        float persistence = .1f;
+
+    public void rebuildMesh(float startX, float startY, float startZ) {
+
+        float persistence = 0.05f;
         int seed = (int)System.currentTimeMillis();
         SimplexNoise noise = new SimplexNoise(CHUNK_SIZE, persistence, seed);
+
         VBOColorHandle = glGenBuffers();
-        VBOVertexHandle = glGenBuffers();
         VBOTextureHandle = glGenBuffers();
-        
-        FloatBuffer VertexPositionData = BufferUtils.createFloatBuffer((CHUNK_SIZE * CHUNK_SIZE * CHUNK_SIZE)*6*12);
-        FloatBuffer VertexColorData    = BufferUtils.createFloatBuffer((CHUNK_SIZE * CHUNK_SIZE * CHUNK_SIZE)*6*12);
-        FloatBuffer VertexTextureData  = BufferUtils.createFloatBuffer((CHUNK_SIZE * CHUNK_SIZE * CHUNK_SIZE)*6*12);
-        
-        for(float x = 0; x < CHUNK_SIZE; x++)
-        {
-            for(float z = 0; z < CHUNK_SIZE; z++)
-            {
-                float height = 1+Math.abs((startY + CHUNK_SIZE/2.0f + (int) (100 * noise.getNoise(calcNoise(x,startX,640), calcNoise(z,startZ,480)))));
-                    
-                for(float y = 0; y < height; y++)
-                {
-                    Blocks[(int)(x)][(int)(y)][(int) (z)] = new Block(Block.BlockType.BlockType_DIRT);
-                    VertexPositionData.put(createCube((startX + x * CUBE_LENGTH), (y * CUBE_LENGTH +(float)(CHUNK_SIZE * -1.5)),(startZ + z * CUBE_LENGTH) + (float)(CHUNK_SIZE*1.5)));
-                    VertexColorData.put(createCubeVertexCol(getCubeColor(Blocks[(int)x][(int)y][(int)z])));
-                    VertexTextureData.put(createTexCube(0, 0, Blocks[(int)(x)][(int)(y)][(int) (z)], y, height));
+        VBOVertexHandle = glGenBuffers();
+
+        FloatBuffer VertexPositionData = BufferUtils.createFloatBuffer((CHUNK_SIZE * CHUNK_SIZE * CHUNK_SIZE) * 6 * 12);
+        FloatBuffer VertexColorData = BufferUtils.createFloatBuffer((CHUNK_SIZE * CHUNK_SIZE * CHUNK_SIZE) * 6 * 12);
+        FloatBuffer VertexTextureData = BufferUtils.createFloatBuffer((CHUNK_SIZE * CHUNK_SIZE *CHUNK_SIZE)* 6 * 12);
+
+        for (float x = 0; x < CHUNK_SIZE; x += 1) {
+            for (float z = 0; z < CHUNK_SIZE; z += 1) {
+                for (float y = 0; y < CHUNK_SIZE; y += 1) {
+                    float height = (float) (startY + (100 * noise.getNoise(x, 0, z)) * CUBE_LENGTH);
+
+                    if (y <= height) {
+                        Blocks[(int)(x)][(int)(y)][(int) (z)] = new Block(Block.BlockType.BlockType_GRASS);
+                        VertexPositionData.put(createCube((float) (startX + x * CUBE_LENGTH), (float) (y * CUBE_LENGTH + (int) (CHUNK_SIZE * .8)), (float) (startZ + z * CUBE_LENGTH)));
+                        VertexColorData.put(createCubeVertexCol(getCubeColor(Blocks[(int) x][(int) y][(int) z])));
+                        VertexTextureData.put(createTexCube((float) 0, (float) 0, Blocks[(int) (x)][(int) (y)][(int) (z)], y, height));
+                    }
                 }
             }
         }
-        
-        fillWater(VertexPositionData, VertexColorData, VertexTextureData, startX, startZ);
-        
-        
-        VertexTextureData.flip();
+        for (float x = 0; x < CHUNK_SIZE; x += 1) {
+            for (float z = 0; z < CHUNK_SIZE; z += 1) {
+                for (float y = 0; y < 2; y += 1) {
+                    if (Blocks[(int)x][(int)y][(int)z] == null) {
+                        Blocks[(int)(x)][(int)(y)][(int) (z)] = new Block(Block.BlockType.BlockType_SAND);
+                        VertexPositionData.put(createCube((float) (startX + x * CUBE_LENGTH), (float) (y * CUBE_LENGTH + (int) (CHUNK_SIZE * .8)), (float) (startZ + z * CUBE_LENGTH)));
+                        VertexColorData.put(createCubeVertexCol(getCubeColor(Blocks[(int) x][(int) y][(int) z])));
+                        if(y == 0)
+                            VertexTextureData.put(texHelper(0,0,5));
+                        else
+                            VertexTextureData.put(texHelper(0,0,4));
+                        Blocks[(int)x][(int)y][(int)z].SetActive(true);
+                    }
+                }
+            }
+        }
+        for (float x = 0; x < CHUNK_SIZE; x += 1) {
+            for (float z = 0; z < CHUNK_SIZE; z += 1) {
+                for (float y = 0; y < 3; y += 1) {
+                    if (Blocks[(int)x][(int)y][(int)z] == null) {
+                        Blocks[(int)(x)][(int)(y)][(int) (z)] = new Block(Block.BlockType.BlockType_WATER);
+                        VertexPositionData.put(createCube((float) (startX + x * CUBE_LENGTH), (float) (y * CUBE_LENGTH + (int) (CHUNK_SIZE * .8)), (float) (startZ + z * CUBE_LENGTH)));
+                        VertexColorData.put(createCubeVertexCol(getCubeColor(Blocks[(int) x][(int) y][(int) z])));
+                        VertexTextureData.put(texHelper(0,0,2));
+                        Blocks[(int)x][(int)y][(int)z].SetActive(true);
+                    }
+                }
+            }
+        }
         VertexColorData.flip();
         VertexPositionData.flip();
-        
-        glBindBuffer(GL_ARRAY_BUFFER, VBOVertexHandle);
-        glBufferData(GL_ARRAY_BUFFER, VertexPositionData, GL_STATIC_DRAW);
+        VertexTextureData.flip();
+
+        glBindBuffer(GL_ARRAY_BUFFER,VBOVertexHandle);
+        glBufferData(GL_ARRAY_BUFFER,VertexPositionData,GL_STATIC_DRAW);
         glBindBuffer(GL_ARRAY_BUFFER, 0);
-        glBindBuffer(GL_ARRAY_BUFFER, VBOColorHandle);
-        glBufferData(GL_ARRAY_BUFFER, VertexColorData, GL_STATIC_DRAW);
-        glBindBuffer(GL_ARRAY_BUFFER, 0);
+        glBindBuffer(GL_ARRAY_BUFFER,VBOColorHandle);
+        glBufferData(GL_ARRAY_BUFFER,VertexColorData,GL_STATIC_DRAW);
+        glBindBuffer(GL_ARRAY_BUFFER, 0); 
         glBindBuffer(GL_ARRAY_BUFFER, VBOTextureHandle);
-        glBufferData(GL_ARRAY_BUFFER, VertexTextureData,GL_STATIC_DRAW);
+        glBufferData(GL_ARRAY_BUFFER, VertexTextureData, GL_STATIC_DRAW);
         glBindBuffer(GL_ARRAY_BUFFER, 0);
     }
-    
+
+
     private float[] createCubeVertexCol(float[] CubeColorArray) {
         float[] cubeColors = new float[CubeColorArray.length * 4 * 6];
         for (int i = 0; i < cubeColors.length; i++) {
@@ -89,28 +115,7 @@ public class Chunk {
         }
         return cubeColors;
     }
-    
-    private void fillWater(FloatBuffer PositionBuff, FloatBuffer ColorBuff,FloatBuffer TextureBuff, float startX, float startZ){
-        for(int x = 0; x < CHUNK_SIZE; x++)
-        {
-            for(int z = 0; z < CHUNK_SIZE; z++)
-            {
-                for(int y = 0; y < (CHUNK_SIZE/2.0f)-1; y++)
-                {
-                    if(Blocks[x][y][z] == null)
-                    {
-                        Blocks[(int)(x)][(int)(y)][(int) (z)] = new Block(Block.BlockType.BlockType_WATER);
-                        PositionBuff.put(createCube((startX + x * CUBE_LENGTH), (y*CUBE_LENGTH+(float)(CHUNK_SIZE*-1.5)),(startZ+z*CUBE_LENGTH) + (float)(CHUNK_SIZE*1.5)));// + (float)(CHUNK_SIZE*1.5)));
-                        ColorBuff.put(createCubeVertexCol(getCubeColor(Blocks[(int)x][(int)y][(int)z])));
-                        TextureBuff.put(texHelper(0,0,2));  
-                        Blocks[x][y][z].SetActive(true);
-                    }
 
-                }
-            }
-        }
-    }
-    
     public static float[] createCube(float x, float y, float z) {
         int offset = CUBE_LENGTH / 2;
         return new float[] {
@@ -145,61 +150,55 @@ public class Chunk {
         x + offset, y - offset, z - CUBE_LENGTH,
         x + offset, y - offset, z };
     }
-    
+
     private float[] getCubeColor(Block block) {
         return new float[] { 1, 1, 1 };
     }
-    
+
     public Chunk(int startX, int startY, int startZ) {
-        
+
         try {
             texture = TextureLoader.getTexture("PNG", ResourceLoader.getResourceAsStream("terrain.png"));
         } catch (Exception e) {
-            System.out.print("Could not load png");
+            System.out.print("ER-ROAR!");
             e.printStackTrace();
         }
         
         Blocks = new Block[CHUNK_SIZE][CHUNK_SIZE][CHUNK_SIZE];
-        
+
+
         VBOColorHandle = glGenBuffers();
         VBOVertexHandle = glGenBuffers();
         VBOTextureHandle = glGenBuffers();
-        
         StartX = startX;
         StartY = startY;
         StartZ = startZ;
-        
         rebuildMesh(startX, startY, startZ);
     }
-    
 
 
     public static float[] createTexCube(float x, float y, Block block, float currY, float height){
         block.SetActive(true);
-        float posY = currY/height;
-        if(currY == height-2)
-            return texHelper(x,y,3);
-        else if(currY == height-1){
-            if(currY < (int) (CHUNK_SIZE/2.0f))
-                return texHelper(x,y,1);
-            else
-                return texHelper(x,y,0);
+        float level = currY/height;
+        if(currY == 2) {
+            return texHelper(x,y,1);
         }
-             
-        
-        if(posY <= 0.2)
+
+        if(level <= 0){
             return texHelper(x,y,5);
-        else if(posY <= 0.7)
+        }
+        else if(level <= 0.5){
             return texHelper(x,y,4);
-        else if(posY <= 1)
+        }
+        else if(level <= 0.8){
             return texHelper(x,y,3);
-        else
-            System.out.println("not found");
-            return null; 
-    }
-    
-    public static int calcNoise(float currX, float startVal, float offset){
-       return(int)(startVal + currX * ((300 - startVal)/ offset));
+        }
+        else if(level <= 1){
+            return texHelper(x,y,0);
+        }
+        else{
+            return texHelper(x,y,0);
+        }
     }
     public static float[] texHelper(float x, float y, int id){
         float offset = (1024f/16)/1024f;
